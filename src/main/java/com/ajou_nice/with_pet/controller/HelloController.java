91c1ce200478d2227b47f1069e3f7f48db1fc5e6
strongcookdas <goodjav141592@gmail.com>
2023-04-29 17:21:07 +0900

    fix : HelloController 수정 - Api 어노테이션 사용하여 Swagger 문서 수정

--- src/main/java/com/ajou_nice/with_pet/controller/HelloController.java
+++ src/main/java/com/ajou_nice/with_pet/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.ajou_nice.with_pet.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/hello")
+@Api(tags = "Test API")
 public class HelloController {
 
     @Value("${ajou.nice}")
     private String test;
 
     @GetMapping
+    @ApiOperation(value = "CICD 테스트 API")
     public String hello() {
         return "hello "+test;
     }
