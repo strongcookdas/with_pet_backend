a31f42b7ebfee1969d69c217ff9cc7001081353b
strongcookdas <goodjav141592@gmail.com>
2023-04-29 17:16:03 +0900

    add : SwaggerConfig 클래스 추가 - swagger 설정정보에 관한 클래스 추가

---
+++ src/main/java/com/ajou_nice/with_pet/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.ajou_nice.with_pet.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(java.sql.Date.class)
+                .forCodeGeneration(true)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.ajou_nice.with_pet.controller"))
+                .build()
+                .apiInfo(apiInfo())
+                .enable(true);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("위드펫 Project Swagger")
+                .description("위드펫 23-1학기 캡스톤 프로젝트")
+                .version("1.0.0")
+                .build();
+    }
+}
