92de64ea9a1afd2a198d17425d7a035c5b59745b
strongcookdas <goodjav141592@gmail.com>
2023-04-30 23:04:38 +0900

    fix : Test api 수정

--- src/main/java/com/ajou_nice/with_pet/controller/HelloController.java
+++ src/main/java/com/ajou_nice/with_pet/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.ajou_nice.with_pet.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class HelloController {
 
     @GetMapping
     @ApiOperation(value = "CICD 테스트 API")
-    public String hello() {
-        return "hello "+test;
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("Hello SpringBoot React!!!!");
     }
 }
