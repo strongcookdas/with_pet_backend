5da08ffc079d6c392b15d44f255a05df3a0fffdd
strongcookdas <goodjav141592@gmail.com>
2023-05-07 16:43:28 +0900

    🐛 fix : cors 문제 해결

--- src/main/java/com/ajou_nice/with_pet/configuration/WebMvcConfig.java
+++ src/main/java/com/ajou_nice/with_pet/configuration/WebMvcConfig.java
@@ -10,7 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ec2-13-125-242-183.ap-northeast-2.compute.amazonaws.com")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("http://localhost:3000",
+                        "http://ec2-13-125-242-183.ap-northeast-2.compute.amazonaws.com")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
