4da46572276d6c2c2a394b66823b55ef43505bd0
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-10 02:09:19 +0900

    🐛 fix: ErrorCode 추가
    - 수정사항 : 펫시터와 지원자에 필요한 ErrorCode 추가

--- src/main/java/com/ajou_nice/with_pet/exception/ErrorCode.java
+++ src/main/java/com/ajou_nice/with_pet/exception/ErrorCode.java
@@ -13,7 +13,11 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 일치하지 않습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
     INVALID_PERMISSION(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
-    DOG_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 반려견이 존재하지 않습니다.");
+    DOG_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 반려견이 존재하지 않습니다."),
+    APPLICANT_NOT_FOUND(HttpStatus.NOT_FOUND, "지원 정보가 없습니다."),
+    DUPLICATED_APPLICATION(HttpStatus.CONFLICT, "중복 지원은 불가능합니다."),
+    BAD_REQUEST_APPLICANT_STATUS(HttpStatus.BAD_REQUEST, "잘못된 지원자 상태 변경 요청입니다."),
+    WITH_PET_SERVICE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 서비스입니다.");
     private HttpStatus status;
     private String message;
 }
