94911a9ec18941e390fd221ae66f1d55402660cc
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-10 02:10:00 +0900

    ✨ feat: 펫시터 지원자 레포지토리 구현

--- src/main/java/com/ajou_nice/with_pet/repository/PetSitterApplicantRepository.java
+++ src/main/java/com/ajou_nice/with_pet/repository/PetSitterApplicantRepository.java
@@ -1,9 +1,13 @@
 package com.ajou_nice.with_pet.repository;
 
 import com.ajou_nice.with_pet.domain.entity.PetSitterApplicant;
+import com.ajou_nice.with_pet.domain.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetSitterApplicantRepository extends JpaRepository<PetSitterApplicant, Long> {
 	Optional<PetSitterApplicant> findById(Long id);
+
+	Optional<PetSitterApplicant> findByUser(User user);
+
 }
