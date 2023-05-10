0bb9953f871971963a79ce8173c1097b7f3e1b86
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-11 03:29:11 +0900

    ✨ feat: 쿼리 dsl을 위한 커스텀 레포지토리 구현

--- src/main/java/com/ajou_nice/with_pet/repository/PetSitterRespositoryCustom.java
+++ src/main/java/com/ajou_nice/with_pet/repository/PetSitterRespositoryCustom.java
@@ -1,10 +1,10 @@
 package com.ajou_nice.with_pet.repository;
 
-import com.ajou_nice.with_pet.domain.dto.petsitter.PetSitterMainDto;
+import com.ajou_nice.with_pet.domain.entity.PetSitter;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PetSitterRespositoryCustom {
-	Page<PetSitterMainDto> searchPage(Pageable pageable);
+	Page<PetSitter> searchPage(Pageable pageable, String userId);
 }
