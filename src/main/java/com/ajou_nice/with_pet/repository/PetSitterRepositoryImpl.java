bbc00670a75994e4011b9d9a1f87e0c0a02688f8
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-11 00:52:53 +0900

    for merge

--- src/main/java/com/ajou_nice/with_pet/repository/PetSitterRepositoryImpl.java
+++ src/main/java/com/ajou_nice/with_pet/repository/PetSitterRepositoryImpl.java
@@ -1,6 +1,10 @@
 package com.ajou_nice.with_pet.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 public class PetSitterRepositoryImpl implements PetSitterRespositoryCustom{
 
-	private final JPAQuery
+	private final JPAQueryFactory queryFactory;
+
+	public
 }
