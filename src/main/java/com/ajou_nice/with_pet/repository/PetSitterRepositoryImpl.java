071b061e8f24f32baf67665d3a6350fdcdc0cc52
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-11 03:28:15 +0900

    ✨ feat: 펫시터 커스텀 레포지토리 implement 레포지토리 구현 - 페이징 로직 구현

--- src/main/java/com/ajou_nice/with_pet/repository/PetSitterRepositoryImpl.java
+++ src/main/java/com/ajou_nice/with_pet/repository/PetSitterRepositoryImpl.java
@@ -1,10 +1,36 @@
 package com.ajou_nice.with_pet.repository;
 
+import static com.ajou_nice.with_pet.domain.entity.QPetSitter.petSitter;
+import static com.ajou_nice.with_pet.domain.entity.QUser.user;
+
+import com.ajou_nice.with_pet.domain.entity.PetSitter;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
-public class PetSitterRepositoryImpl implements PetSitterRespositoryCustom{
+@Slf4j
+public class PetSitterRepositoryImpl extends QuerydslRepositorySupport implements PetSitterRespositoryCustom{
 
-	private final JPAQueryFactory queryFactory;
+	@Autowired
+	private JPAQueryFactory queryFactory;
 
-	public
+	public PetSitterRepositoryImpl(){
+		super(PetSitter.class);
+	}
+	@Override
+	public Page<PetSitter> searchPage(Pageable pageable, String userId){
+		List<PetSitter> petSitters = queryFactory.select(petSitter)
+				.from(petSitter, user)
+				.where(petSitter.applicant.user.id.eq(userId)).fetch();
+
+		int start = (int) pageable.getOffset();
+		int end = Math.min((start+ pageable.getPageSize()), petSitters.size());
+		return new PageImpl<PetSitter>(petSitters.subList(start,end), pageable, petSitters.size());
+	}
+
 }
