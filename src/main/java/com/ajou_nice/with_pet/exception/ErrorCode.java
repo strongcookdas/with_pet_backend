093545775829775dad23d963e9a711ce460f72cf
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-14 02:38:15 +0900

    🐛 fix : 에러 코드 추가

--- src/main/java/com/ajou_nice/with_pet/exception/ErrorCode.java
+++ src/main/java/com/ajou_nice/with_pet/exception/ErrorCode.java
@@ -19,7 +19,10 @@ public enum ErrorCode {
     BAD_REQUEST_APPLICANT_STATUS(HttpStatus.BAD_REQUEST, "잘못된 지원자 상태 변경 요청입니다."),
     WITH_PET_SERVICE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 서비스입니다."),
     PETSITTER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 펫시터가 없습니다."),
-    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 그룹이 존재하지 않습니다." );
+    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 그룹이 존재하지 않습니다." ),
+    PETSITTER_SERVICE_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 서비스입니다."),
+    PETSITTER_HOUSE_NOT_FOUND(HttpStatus.NOT_FOUND, "등록된 집 정보가 없습니다."),
+    PETSITTER_HASHTAG_NOT_FOUND(HttpStatus.NOT_FOUND, "등록된 해시태그가 없습니다.");
     private HttpStatus status;
     private String message;
 }
