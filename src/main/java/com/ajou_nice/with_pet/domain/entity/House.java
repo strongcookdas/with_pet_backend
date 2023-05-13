d6900712f20d9c9d891d3b984cae75de0a9f0738
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-14 02:34:31 +0900

    🐛 fix: 펫시터 House entity 수정 - 수정 내용 : 대표를 표현하는 representative 변수 추가

--- src/main/java/com/ajou_nice/with_pet/domain/entity/House.java
+++ src/main/java/com/ajou_nice/with_pet/domain/entity/House.java
@@ -1,17 +1,26 @@
 package com.ajou_nice.with_pet.domain.entity;
 
 
+import com.ajou_nice.with_pet.domain.dto.petsitter.PetSitterRequest.PetSitterHouseRequest;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@ToString
 public class House {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +31,18 @@ public class House {
 	private PetSitter petSitter;
 
 	private String house_img;
+
+	private Boolean representative;
+
+
+	public static House toEntity(PetSitter petSitter,PetSitterHouseRequest petSitterHouseRequest){
+		return House.builder()
+				.petSitter(petSitter)
+				.house_img(petSitterHouseRequest.getHouseImg())
+				.representative(petSitterHouseRequest.getRepresentative())
+				.build();
+	}
+
+
 
 }
