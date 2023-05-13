1ba3593243974091abdc7dada542b852b3778dad
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-14 02:42:10 +0900

    🐛 fix: 펫시터 서비스 레포지토리 수정 - Query문 2개 추가 - 작동여부 확인 필요

--- src/main/java/com/ajou_nice/with_pet/repository/PetSitterServiceRepository.java
+++ src/main/java/com/ajou_nice/with_pet/repository/PetSitterServiceRepository.java
@@ -1,8 +1,20 @@
 package com.ajou_nice.with_pet.repository;
 
+import com.ajou_nice.with_pet.domain.entity.PetSitter;
 import com.ajou_nice.with_pet.domain.entity.PetSitterWithPetService;
+import com.ajou_nice.with_pet.service.PetSitterService;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PetSitterServiceRepository extends JpaRepository<PetSitterWithPetService, Long> {
 
+
+	@Query("select s from PetSitterWithPetService s where s.petSitter.id=:petSitterId")
+	Optional<List<PetSitterService>> findAllByPetSitterInQuery(@Param("petSitterId") Long petSitterId);
+
+	@Query("delete from PetSitterWithPetService p where p.petSitter.id=:petSitterId")
+	void deleteAllByPetSitterInQuery(@Param("petSitterId") Long petSitterId);
 }
