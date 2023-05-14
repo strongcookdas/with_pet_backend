28ad65156f850e3d02271d2c3d16a9fe4114464f
j-seunghyun <jason5102@ajou.ac.kr>
2023-05-14 21:28:33 +0900

    🐛 fix: 펫시터 메인 응답 DTO 수정 - 펫시터 필수 서비스 DTO 응답 추가

--- src/main/java/com/ajou_nice/with_pet/domain/dto/petsitter/PetSitterMainResponse.java
+++ src/main/java/com/ajou_nice/with_pet/domain/dto/petsitter/PetSitterMainResponse.java
@@ -2,6 +2,8 @@ package com.ajou_nice.with_pet.domain.dto.petsitter;
 
 
 import com.ajou_nice.with_pet.domain.entity.PetSitter;
+import com.ajou_nice.with_pet.exception.AppException;
+import com.ajou_nice.with_pet.exception.ErrorCode;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,16 +24,21 @@ public class PetSitterMainResponse {
 
 	private String petSitterRepresentativeHouse;
 
-	private String introduction;
+	private int review_count;
+	private Double star_rate;
 
 	public static PetSitterMainResponse of(PetSitter petSitter){
 		return PetSitterMainResponse.builder()
 				.petSitterId(petSitter.getId())
 				.petSitterRepresentativeHouse(petSitter.getPetSitterHouseList()
-						.stream().filter(house->house.getRepresentative().equals("true")).findAny().orElse(null)
+						.stream().filter(house->house.getRepresentative().equals(true)).findAny().orElseThrow(()->{
+							throw new AppException(ErrorCode.PETSITTER_MAIN_HOUSE_NOT_FOUND,
+									ErrorCode.PETSITTER_MAIN_HOUSE_NOT_FOUND.getMessage());
+						})
 						.getHouse_img())
 				.userName(petSitter.getApplicant().getUser().getName())
-				.introduction(petSitter.getIntroduction())
+				.review_count(petSitter.getReview_count())
+				.star_rate(petSitter.getStar_rate())
 				.build();
 	}
 }
