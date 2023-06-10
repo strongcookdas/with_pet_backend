4e9babad99b85b6f3dd1b405a9bff31e23dca7f5
strongcookdas <goodjav141592@gmail.com>
2023-06-10 20:53:49 +0900

    feat : 휴대폰 인증 번호 발급 제한 기능 구현

--- src/main/java/com/ajou_nice/with_pet/controller/SMSController.java
+++ src/main/java/com/ajou_nice/with_pet/controller/SMSController.java
@@ -2,6 +2,8 @@ package com.ajou_nice.with_pet.controller;
 
 import com.ajou_nice.with_pet.domain.dto.Response;
 import com.ajou_nice.with_pet.service.SMSService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,9 @@ public class SMSController {
     private final SMSService smsService;
 
     @GetMapping
-    public Response sendOne(@RequestParam String to){
-        smsService.sendOne(to);
-        return Response.success("인증번호를 발송했습니다.");
+    public Response sendOne(@RequestParam String to, HttpServletRequest request,
+            HttpServletResponse response) {
+        String random = smsService.sendOne(to, request, response);
+        return Response.success(random);
     }
 }
--- src/main/java/com/ajou_nice/with_pet/service/SMSService.java
+++ src/main/java/com/ajou_nice/with_pet/service/SMSService.java
@@ -1,18 +1,26 @@
 package com.ajou_nice.with_pet.service;
 
+import com.ajou_nice.with_pet.exception.AppException;
+import com.ajou_nice.with_pet.exception.ErrorCode;
+import com.ajou_nice.with_pet.utils.CookieUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SMSService {
 
+    private final Integer smsCount = 5;
+    private final CookieUtil cookieUtil;
     @Value("${SMS.KEY}")
     private String api_key;
 
@@ -22,18 +30,32 @@ public class SMSService {
     @Value("${SMS.PHONE}")
     private String from;
 
-    private String randNum(){
+    private String randNum() {
         return RandomStringUtils.randomNumeric(6);
     }
 
-    public SingleMessageSentResponse sendOne(String to){
-        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(api_key, api_secret, "https://api.coolsms.co.kr");
+    public String sendOne(String to, HttpServletRequest request, HttpServletResponse response) {
+
+        String sms = cookieUtil.getCookieValue(request, "sms");
+        if (sms == null) {
+            cookieUtil.addCookie(response, "sms", String.valueOf(1), "/");
+        } else {
+            if (Integer.parseInt(sms) > 5) {
+                throw new AppException(ErrorCode.TOO_MANY_SMS, ErrorCode.TOO_MANY_SMS.getMessage());
+            }
+            cookieUtil.addCookie(response, "sms", String.valueOf(Integer.parseInt(sms) + 1), "/");
+        }
+
+        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(api_key, api_secret,
+                "https://api.coolsms.co.kr");
         Message message = new Message();
         message.setFrom(from);
         message.setTo(to);
-        message.setText("[위드펫] 인증번호: "+ this.randNum());
+        String random = this.randNum();
+        message.setText("[위드펫] 인증번호: " + random);
 
-        SingleMessageSentResponse response = messageService.sendOne(new SingleMessageSendingRequest(message));
-        return response;
+        SingleMessageSentResponse messageSentResponse = messageService.sendOne(
+                new SingleMessageSendingRequest(message));
+        return random;
     }
 }
