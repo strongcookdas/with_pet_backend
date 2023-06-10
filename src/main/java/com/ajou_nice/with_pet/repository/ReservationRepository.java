b3430b456f5a16c4d71e4e98987d96183261b8d9
strongcookdas <goodjav141592@gmail.com>
2023-06-10 13:02:29 +0900

    add : DogDeleteTest에 필요한 메서드 추가

--- src/main/java/com/ajou_nice/with_pet/repository/ReservationRepository.java
+++ src/main/java/com/ajou_nice/with_pet/repository/ReservationRepository.java
@@ -67,4 +67,5 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long>,
     @Query("select r from Reservation r where r.tid=:tid")
     Optional<Reservation> findByTid(@Param("tid") String tid);
 
+    Boolean existsByDogAndReservationStatusIn(Dog dog,List<ReservationStatus> statuses);
 }
