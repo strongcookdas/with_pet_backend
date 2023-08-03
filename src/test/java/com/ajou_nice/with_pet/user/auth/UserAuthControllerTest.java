264ab71bade3f764b014df24a209511b28ca692f
strongcookdas <goodjav141592@gmail.com>
2023-08-03 14:56:55 +0900

    ✅	test : 로그인 컨트롤러 성공 테스트 케이스 추가

---
+++ src/test/java/com/ajou_nice/with_pet/user/auth/UserAuthControllerTest.java
@@ -0,0 +1,63 @@
+package com.ajou_nice.with_pet.user.auth;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.ajou_nice.with_pet.controller.user.UserAuthController;
+import com.ajou_nice.with_pet.domain.dto.auth.UserLoginRequest;
+import com.ajou_nice.with_pet.domain.dto.auth.UserLoginResponse;
+import com.ajou_nice.with_pet.enums.UserRole;
+import com.ajou_nice.with_pet.service.user.UserAuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.awt.PageAttributes.MediaType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserAuthController.class)
+public class UserAuthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserAuthService userAuthService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    UserLoginRequest userLoginRequest;
+    UserLoginResponse userLoginResponse;
+
+    @BeforeEach
+    public void setup(){
+        userLoginResponse = UserLoginResponse.builder().userName("some1")
+                .userProfile("userProfile")
+                .role(UserRole.ROLE_USER.name())
+                .build();
+
+        userLoginRequest = UserLoginRequest.builder().email("email@email.com")
+                .password("password")
+                .build();
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    void login_success() {
+        when(userAuthService.login(any(),any()))
+                .thenReturn(userLoginResponse);
+
+        mockMvc.perform(post("/api/v2/login")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(userLoginRequest))
+                .andDo(print())
+                .andExpect(stauts().isOk()));
+    }
+}
