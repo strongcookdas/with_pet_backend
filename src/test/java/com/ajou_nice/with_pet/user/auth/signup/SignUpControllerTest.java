7b7a3398e3910e90d39aed5ee93d595840fcef6a
strongcookdas <goodjav141592@gmail.com>
2023-08-04 02:36:16 +0900

    ✅	test : 회원가입 테스트코드 작성

--- src/test/java/com/ajou_nice/with_pet/user/auth/signup/SignUpControllerTest.java
+++ src/test/java/com/ajou_nice/with_pet/user/auth/signup/SignUpControllerTest.java
@@ -1,5 +1,82 @@
 package com.ajou_nice.with_pet.user.auth.signup;
 
-public class SignUpControllerTest {
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ajou_nice.with_pet.CommonApiTest;
+import com.ajou_nice.with_pet.controller.user.UserAuthController;
+import com.ajou_nice.with_pet.domain.dto.auth.UserSignUpRequest;
+import com.ajou_nice.with_pet.domain.dto.auth.UserSignUpResponse;
+import com.ajou_nice.with_pet.domain.dto.embedded.AddressDto;
+import com.ajou_nice.with_pet.service.user.UserAuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserAuthController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+public class SignUpControllerTest extends CommonApiTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserAuthService userAuthService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    UserSignUpRequest userSignUpRequest;
+    UserSignUpResponse userSignUpResponse;
+    AddressDto address;
+
+    @BeforeEach
+    public void setUp(){
+        address = AddressDto.builder()
+                .zipcode("111111")
+                .streetAdr("월드컵로")
+                .detailAdr("팔달관")
+                .build();
+
+        userSignUpRequest = UserSignUpRequest.builder()
+                .userName("user1")
+                .userEmail("email@email.com")
+                .userPassword("password")
+                .userPasswordCheck("password")
+                .profileImg("image")
+                .phoneNum("010-0000-0000")
+                .address(address)
+                .build();
+
+        userSignUpResponse = UserSignUpResponse.builder()
+                .userId(1L)
+                .userName("user1")
+                .build();
+    }
+
+    @Test
+    @DisplayName("회원가입")
+    @WithMockUser
+    void signUp_success() throws Exception {
+        when(userAuthService.signUp(userSignUpRequest))
+                .thenReturn(userSignUpResponse);
+
+        mockMvc.perform(post("/api/v2/users/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(userSignUpRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
---
+++ src/test/java/com/ajou_nice/with_pet/user/auth/signup/SignUpServiceTest.java
@@ -0,0 +1,121 @@
+package com.ajou_nice.with_pet.user.auth.signup;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.ajou_nice.with_pet.domain.dto.auth.UserSignUpRequest;
+import com.ajou_nice.with_pet.domain.dto.auth.UserSignUpResponse;
+import com.ajou_nice.with_pet.domain.dto.embedded.AddressDto;
+import com.ajou_nice.with_pet.domain.entity.User;
+import com.ajou_nice.with_pet.enums.UserRole;
+import com.ajou_nice.with_pet.exception.AppException;
+import com.ajou_nice.with_pet.exception.ErrorCode;
+import com.ajou_nice.with_pet.repository.UserRepository;
+import com.ajou_nice.with_pet.service.ValidateCollection;
+import com.ajou_nice.with_pet.service.user.UserAuthService;
+import com.ajou_nice.with_pet.utils.JwtTokenUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SignUpServiceTest {
+    UserAuthService userAuthService;
+    UserRepository userRepository = mock(UserRepository.class);
+    BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+    JwtTokenUtil jwtTokenUtil = mock(JwtTokenUtil.class);
+    ValidateCollection validateCollection = mock(ValidateCollection.class);
+
+    UserSignUpRequest userSignUpRequest;
+    UserSignUpResponse userSignUpResponse;
+    AddressDto address;
+    User user;
+
+    @BeforeEach
+    public void setUp(){
+        address = AddressDto.builder()
+                .zipcode("111111")
+                .streetAdr("월드컵로")
+                .detailAdr("팔달관")
+                .build();
+
+        userSignUpResponse = UserSignUpResponse.builder()
+                .userId(1L)
+                .userName("user1")
+                .build();
+
+        user = User.builder()
+                .userId(1L)
+                .name("user1")
+                .email("email@email.com")
+                .password("password")
+                .role(UserRole.ROLE_USER)
+                .profileImg("image")
+                .phone("010-0000-0000")
+                .build();
+    }
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void signUp_success() {
+        //given
+        userSignUpRequest = UserSignUpRequest.builder()
+                .userName("user1")
+                .userEmail("email@email.com")
+                .userPassword("password")
+                .userPasswordCheck("password")
+                .profileImg("image")
+                .phoneNum("010-0000-0000")
+                .address(address)
+                .build();
+        //when
+        when(userRepository.save(user))
+                .thenReturn(user);
+        //then
+        UserSignUpResponse result = userAuthService.signUp(userSignUpRequest);
+        assertEquals(result.getUserId(),user.getUserId());
+        assertEquals(result.getUserName(),user.getName());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패1 : 아이디 중복")
+    void signUp_fail1() {
+        //given
+        userSignUpRequest = UserSignUpRequest.builder()
+                .userName("user1")
+                .userEmail("email@email.com")
+                .userPassword("password")
+                .userPasswordCheck("password")
+                .profileImg("image")
+                .phoneNum("010-0000-0000")
+                .address(address)
+                .build();
+        //when
+        when(validateCollection.userValidation(userSignUpRequest.getUserEmail()))
+                .thenThrow(new AppException(ErrorCode.DUPLICATED_USER_ID,ErrorCode.DUPLICATED_USER_ID.getMessage()));
+        //then
+        AppException exception = assertThrows(AppException.class,() -> userAuthService.signUp(userSignUpRequest));
+        assertEquals(ErrorCode.DUPLICATED_USER_ID,exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패2 : 패스워드 불일치")
+    void signUp_fail2() {
+        //given
+        userSignUpRequest = UserSignUpRequest.builder()
+                .userName("user1")
+                .userEmail("email@email.com")
+                .userPassword("password1")
+                .userPasswordCheck("password2")
+                .profileImg("image")
+                .phoneNum("010-0000-0000")
+                .address(address)
+                .build();
+        //when
+        //then
+        AppException exception = assertThrows(AppException.class,() -> userAuthService.signUp(userSignUpRequest));
+        assertEquals(ErrorCode.PASSWORD_COMPARE_FAIL,exception.getErrorCode());
+    }
+}
