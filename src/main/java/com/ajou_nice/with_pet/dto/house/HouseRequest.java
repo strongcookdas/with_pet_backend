2cbdf4a4aa6435a7958322fa2fcb07ee8df6bbf3
strongcookdas <goodjav141592@gmail.com>
2023-08-11 16:36:56 +0900

    🐛 fix : HouseRequest valid 어노테이션 추가

--- src/main/java/com/ajou_nice/with_pet/dto/house/HouseRequest.java
+++ src/main/java/com/ajou_nice/with_pet/dto/house/HouseRequest.java
@@ -1,5 +1,6 @@
 package com.ajou_nice.with_pet.dto.house;
 
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +13,9 @@ import lombok.ToString;
 @Builder
 @ToString
 public class HouseRequest {
+
+    @NotNull(message = "이미지를 업로드해주세요.")
     private String houseImg;
+    @NotNull(message = "대표사진 여부값을 입력해주세요.")
     private Boolean isRepresentative;
 }
